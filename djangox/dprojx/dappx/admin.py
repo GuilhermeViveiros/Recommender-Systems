1d717268978717c6f506950b692797caf388be52
Sofia <sofia@pop-os.localdomain>
2019-12-21 20:26:40 +0000

    dataset too big

---
+++ djangox/dprojx/dappx/admin.py
@@ -0,0 +1,8 @@
+# dappx/admin.py
+
+from django.contrib import admin
+from dappx.models import User, Movie #UserProfileInfo, 
+
+# Register your models here.
+#admin.site.register(UserProfileInfo)
+admin.site.register(Movie)
---
+++ mysite/newenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sofia/Uni/SBS/RecommenderSystems/mysite/newenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
